e935ff74e9e1ac90dc1ebb302dd0a68081f080bd
CoghHs <103933833+CoghHs@users.noreply.github.com>
2023-08-20 18:08:09 +0900

    react-basename

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 import "./styles.css";
 function App() {
 	return (
-		<Router>
+		<Router basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/movie/:id" element={<Detail />} />
